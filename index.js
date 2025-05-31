7aa9a1982b568f8eae91d06a62ef192cbee81d28
Kanamu162A <muabubakar715@gmail.com>
2025-05-31 17:32:51 +0100

    Added root route

--- index.js
+++ index.js
@@ -3,30 +3,29 @@ import dotenv from "dotenv";
 import cors from "cors";
 import auth_router from "./router/auth.router.js";
 import users_route from "./router/dashboard.router.js";
-import  ResetPin  from "./router/reset.route.js"
-
+import ResetPin from "./router/reset.route.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 const app = express();
-const port =  process.env.PORT || 5000;
-app.use(express.static(path.join(__dirname, 'public')));
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
-  
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/shatova/V1/",auth_router);
-app.use("/api/shatova/V1/",users_route);
-app.use("/api/shatova/V1/",ResetPin);
+app.use("/api/shatova/V1/", auth_router);
+app.use("/api/shatova/V1/", users_route);
+app.use("/api/shatova/V1/", ResetPin);
 
-
-
-app.listen(port, () =>{
-
-    console.log(`server running on http://localhost:${port} Successfully..`);
-    
+app.listen(port, () => {
+  console.log(`server running on http://localhost:${port} Successfully..`);
 });
