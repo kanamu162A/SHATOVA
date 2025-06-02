b1131c2f76a663b063462f2b967651bbb5682d7b
Kanamu162A <muabubakar715@gmail.com>
2025-06-02 13:25:57 +0100

    index.html

---
+++ middleware/authorize.middleware.js
@@ -0,0 +1,12 @@
+const authorizeRoles = (...allowedRoles) => {
+    return (req, res, next) => {
+      const userRole = req.user.role?.toLowerCase();
+      const isAllowed = allowedRoles.map(r => r.toLowerCase()).includes(userRole);
+      if (!isAllowed) {
+        return res.status(403).json({ message: "Access denied" });
+      }
+      next();
+    };
+  };
+  
+  export default authorizeRoles;
--- router/dashboard.router.js
+++ router/dashboard.router.js
@@ -4,11 +4,12 @@ import {getAllUsers} from "../controller/auth.controller.js"
 import authenticateToken from "../middleware/auth.middleware.js"
 import { CreateAdmin } from "../controller/admin.controller.js";
 import { getDashboard } from "../controller/dashboard.controller.js";
+import authorizeRoles from "../middleware/authorize.middleware.js";
 
 
-router.get("/users",authenticateToken, getAllUsers);
+router.get("/users",authenticateToken,authorizeRoles("admin","C E O"), getAllUsers);
 router.get("/dashboard",authenticateToken,getDashboard)
-router.post("/admin",authenticateToken, CreateAdmin)
+router.post("/admin",authenticateToken,authorizeRoles(" C E O"), CreateAdmin)
 
 
 
