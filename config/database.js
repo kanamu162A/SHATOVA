dae8e89b4ef3c5f1310623d70767db2cfbc16c7a
Kanamu162A <muabubakar715@gmail.com>
2025-06-11 23:56:50 +0100

    from shatova

--- config/database.js
+++ config/database.js
@@ -1,32 +1,32 @@
 import dotenv from "dotenv";
-import pkg from "pg";
-const { Pool } = pkg;
+import pkg from "pg"
+const  {Pool} = pkg;
 
 dotenv.config();
 
-const isProduction = Boolean(process.env.DATABASE_URL);
+const pool =  new Pool({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized:false,
+    },
 
-const pool = isProduction
-  ? new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false, // Required by Render
-      },
-    })
-  : new Pool({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT,
-    });
+});
 
-pool.connect((err) => {
-  if (!err) {
-    console.log(`✅ Database Connected Successfully..`);
-  } else {
-    console.error("❌ Fail to connect to database:", err.message);
-  }
+
+
+pool.connect((err) =>{
+    if(!err){
+        console.log(`Database Connected Successfully..`);
+        
+    }else{
+        console.log('Fail to connect to database ',err.message);
+        
+    };
 });
 
 export default pool;
