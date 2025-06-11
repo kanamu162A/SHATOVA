7c23fd570fdf4f3ff1b317b99b3eeb0d73dea8dd
Kanamu162A <muabubakar715@gmail.com>
2025-06-11 23:47:47 +0100

    from shatova

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
+import { truncates } from "bcryptjs";
 import dotenv from "dotenv";
 import pkg from "pg"
 const  {Pool} = pkg;
@@ -5,11 +6,13 @@ const  {Pool} = pkg;
 dotenv.config();
 
 const pool =  new Pool({
+    
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
     database:process.env.DB_NAME,
     port:process.env.DB_PORT,
+
     ssl: {
         rejectUnauthorized: false
       }
