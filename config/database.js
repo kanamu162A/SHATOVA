75207e52035f06c5df102d79a8f1ef590427a98f
Kanamu162A <muabubakar715@gmail.com>
2025-06-15 05:48:29 +0100

    from manneeru

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ const pool =  new Pool({
     password:process.env.DB_PASSWORD,
     database:process.env.DB_NAME,
     port:process.env.DB_PORT,
-    connectionString: process.env.DATABASE_URL,
+    //connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized:false,
     },
